63a84f0adf828ab385bcd810ce35fcc3b25c163e
Erik Harding <erik@praekelt.org>
2018-07-26 15:43:26 +0200

    fix migration where unicode is expected

--- temba/flows/flow_migrations.py
+++ temba/flows/flow_migrations.py
@@ -214,7 +214,7 @@ def migrate_export_to_version_11_0(json_export, org, same_site=True):
                 if action['type'] in ['reply', 'send', 'say']:
                     msg = action['msg']
                     for lang, text in msg.items():
-                        migrated_text = text
+                        migrated_text = unicode(text)
                         for pattern, replacement in replacements:
                             migrated_text = regex.sub(
                                 pattern,
